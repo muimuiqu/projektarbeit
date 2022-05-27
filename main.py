75ccc1e47b0bb1410ff1641c42f3bc10318cf89c
muimuiqu <muimuiqu@users.noreply.github.com>
2022-05-27 10:18:34 +0200

    teilnahme.html

--- main.py
+++ main.py
@@ -18,15 +18,14 @@ def formular():
         vorname = data["vorname"]
         nachname = data["nachname"]
         datum = data["datum"]
-        anwesenheit_auftritte = data["anwesenheit_auftritte"]
-        anwesenheit_proben = data["anwesenheit_probe"]
+        event = data["event"]
         try:
             with open("aktivitaeten_2.json", "r") as open_file:
                 datei_inhalt = json.load(open_file)
         except FileNotFoundError:
             datei_inhalt = []
 
-        my_dict = {"Vorname": vorname, "Nachname": nachname, "Datum": datum, "Auftritte": anwesenheit_auftritte, "Probe": anwesenheit_proben}
+        my_dict = {"Vorname": vorname, "Nachname": nachname, "Datum": datum, "Event": event, }
         datei_inhalt.append(my_dict)
 
         with open("aktivitaeten_2.json", "w") as open_file:
@@ -38,12 +37,18 @@ def formular():
 
 @app.route("/teilnahme/", methods=['GET', 'POST'])
 def teilnahme():
-    with open("aktivitaeten_2.json") as open_file:
-        json_as_string = open_file.read()
-        my_dict_2 = loads(json_as_string)
-
-
-    return render_template("teilnahme.html")
+    if request.method == "POST":
+        data = request.form
+        vorname = data["vorname"]
+        nachname = data["nachname"]
+        try:
+            with open("aktivitaeten_2.json") as open_file:
+                my_dict_2 = json.load(open_file)
+        except FileNotFoundError:
+            my_dict_2 = {}
+        return my_dict_2
+    else:
+        return render_template("teilnahme.html")
 
 
 @app.route("/jahresprogramm/", methods=['GET', 'POST'])
