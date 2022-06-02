fd9d0feea5013bf18095f489f2e529aede907525
muimuiqu <muimuiqu@users.noreply.github.com>
2022-06-02 16:41:11 +0200

    teilnahme anzeige

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
+from json import loads
+
 
 app = Flask("projektarbeit")
 
@@ -16,7 +18,6 @@ def formular():
     if request.method == "POST":
         data = request.form
         vorname = data["vorname"]
-        nachname = data["nachname"]
         datum = data["datum"]
         event = data["event"]
         try:
@@ -25,36 +26,22 @@ def formular():
         except FileNotFoundError:
             datei_inhalt = []
 
-        my_dict = {"Vorname": vorname, "Nachname": nachname, "Datum": datum, "Event": event, }
+        my_dict = {"Vorname": vorname, "Datum": datum, "Event": event, }
         datei_inhalt.append(my_dict)
 
         with open("aktivitaeten_2.json", "w") as open_file:
             json.dump(datei_inhalt, open_file, indent=4)
-        return str("Besten Dank, deine Daten wurden gesichert")
+        return render_template("ok.html")
     else:
         return render_template("formular.html")
 
 
-@app.route("/teilnahme/", methods=['GET', 'POST'])
+@app.route("/teilnahme/")
 def teilnahme():
-
-    try:
-        with open("aktivitaeten_2.json", "r") as open_file:
-            datei_inhalt = json.load(open_file)
-    except FileNotFoundError:
-        datei_inhalt = []
-
-    return [datei_inhalt]
-
-
-@app.route("/jahresprogramm/", methods=['GET', 'POST'])
-def jahresprogramm():
-    return render_template("jahresprogramm.html")
-
-
-@app.route("/about/", methods=['GET', 'POST'])
-def about():
-    return render_template("about.html")
+    with open("aktivitaeten_2.json") as open_file:
+        json_as_string = open_file.read()
+        daten_inhalt = loads(json_as_string)
+    return render_template("teilnahme.html", daten_inhalt=daten_inhalt)
 
 
 if __name__ == "__main__":
