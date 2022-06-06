0885b56f2ef61e4f3aa999d50b8bbb21ebb30ffc
muimuiqu <muimuiqu@users.noreply.github.com>
2022-06-06 18:50:02 +0200

    JSON Datenbank wiederhergestellt (Fehler bei formular.html) README ergänzt

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
 Problembeschreibung/Motivation
 Seit der 5. Klasse bin ich Mitglied in einem Verein. Das Anwesenheitsprotokoll wird bei uns noch von Hand gemacht und
 ich bin der Meinung, dass die Erfassung der Proben- und Auftrittsbesuche einfacher und schneller erfolgen kann.
+In diesem Projekt wurde die Anwesenheitserfassung von 6 Mitglieder realisiert.
 
 Betrieb
 Welche zusätzliche Pakete müssen bei Bedarf installiert werden. (Muss im Normalfall nicht beachtet werden. Python muss nicht erwähnt werden, da das bei einem Python Projekt impliziert ist.)
 Was muss man bei der Ausführung beachten. Was muss eventuell davor noch gemacht werden.
 Welch Datei muss ausgeführt werden
+Json: import und load (Erstellung und Zugriff der Datenbank)
 
 Benutzung
 Wie wird das Projekt benutzt
 Welche Optionen oder auch Spezialitäten existieren
+Die Idee dahinter ist, dass jedes Mitglied die Erfassung selbstständig nachtragen kann.
+Bei der Übersicht-Seite 2022 ist auf einem Blick erkenntlich, welches Mitglied die häufigsten Events besucht hat.
+Auf diese Art und Weise spart man sich die Zeit, dass jemand aus dem Verein die Mitglieder zählt und per Excel Daten die Events auswerten muss.
+Zusätzlich bekommt man mit einer schönen visuellen Darstellung eine angenehme Sicht.
 
 Architektur
+Home-Seite: Begrüssung der Mitglieder
+Erfassung Anwesenheit-Seite: Proben und Auftritte eintragen
+Teilnahme-Seite: Auflistung der besuchten Events, hier kann jedes Mitglied auf seine eigenen erfassten Daten zugreifen
+Übersicht 2022-Seite: Diagramm Übersicht über die Proben und Auftritte aller Mitglieder
 Hier bei Bedarf eine kurze Beschreibung des Ablaufs des Programms auf Code Ebene z.B. als Ablaufdiagramm.
 
 Ungelöste/unbearbeitete Probleme
 Was wurde nicht gelöst
-Welche Verbesserungen könnten noch gemacht werden.
+Die HTML.Berechnung wäre sicher einfacher zu lösen als die einzelne Auflistung der Mitglieder.
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
+from json import loads
 
 
 app = Flask("projektarbeit")
@@ -12,29 +13,137 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/formular/", methods=["GET", "POST"])
+@app.route("/formular/", methods=['GET', 'POST'])  # Verlinkung Formular
 def formular():
-    if request.method == "POST":
+    if request.method == 'POST':
         data = request.form
         vorname = data["vorname"]
         datum = data["datum"]
         event = data["event"]
-
         try:
-            with open("aktivitaeten.json", "r") as open_file:
+            with open("aktivitaeten_2.json", "r") as open_file:  # r für read = lesen
                 datei_inhalt = json.load(open_file)
         except FileNotFoundError:
             datei_inhalt = []
-
         my_dict = {"Vorname": vorname, "Datum": datum, "Event": event}
         datei_inhalt.append(my_dict)
-
-        with open("aktivitaeten.json", "w") as open_file:
-            json.dump(datei_inhalt, open_file, indent=4)
-            return render_template("ok.html")
-
+        with open("aktivitaeten_2.json", "w") as open_file:
+            json.dump(datei_inhalt, open_file, indent=4)  # indent=4 sieht schöner aussieht
+        return render_template("ok.html")
     else:
         return render_template("formular.html")
+
+
+@app.route("/ok/", methods=["GET", "POST"])
+def ok():
+    return render_template("ok.html")
+
+
+@app.route("/teilnahme/")
+def teilnahme():
+    with open("aktivitaeten_2.json") as open_file:
+        json_as_string = open_file.read()
+        datei_inhalt = loads(json_as_string)
+    return render_template("/teilnahme.html", datei_inhalt=datei_inhalt)
+
+
+@app.route("/berechnung/")
+def berechnung():
+
+    with open("aktivitaeten_2.json", "r") as open_file:
+        json_as_string = open_file.read()
+        datei_inhalt = loads(json_as_string)
+
+    summe_proben_stefanie = 0
+    summe_auftritte_stefanie = 0
+    summe_proben_philipp = 0
+    summe_auftritte_philipp = 0
+    summe_proben_rene = 0
+    summe_auftritte_rene = 0
+    summe_proben_mario = 0
+    summe_auftritte_mario = 0
+    summe_proben_urs = 0
+    summe_auftritte_urs = 0
+    summe_proben_cilli = 0
+    summe_auftritte_cilli = 0
+
+    for value in datei_inhalt:  # Eintrag Proben
+        if value["Vorname"] == "Stefanie":
+            try:
+                summe_proben_stefanie += float(value["event: probe"])
+            except:
+                continue
+        elif value["Vorname"] == "Philipp":
+            try:
+                summe_proben_philipp += float(value["event: probe"])
+            except:
+                continue
+        elif value["Vorname"] == "Rene":
+            try:
+                summe_proben_rene += float(value["event: probe"])
+            except:
+                continue
+        elif value["Vorname"] == "Mario":
+            try:
+                summe_proben_mario += float(value["event: probe"])
+            except:
+                continue
+        elif value["Vorname"] == "Urs":
+            try:
+                summe_proben_urs += float(value["event: probe"])
+            except:
+                continue
+        elif value["Vorname"] == "Cilli":
+            try:
+                summe_proben_cilli += float(value["event: probe"])
+            except:
+                continue
+
+    for value in datei_inhalt:  # Eintrag Auftritte
+        if value["Vorname"] == "Stefanie":
+            try:
+                summe_auftritte_stefanie += float(value["event: auftritte"])
+            except:
+                continue
+        elif value["Vorname"] == "Philipp":
+            try:
+                summe_auftritte_philipp += float(value["event: auftritte"])
+            except:
+                continue
+        elif value["Vorname"] == "Rene":
+            try:
+                summe_auftritte_rene += float(value["event: auftritte"])
+            except:
+                continue
+        elif value["Vorname"] == "Mario":
+            try:
+                summe_auftritte_mario += float(value["event: auftritte"])
+            except:
+                continue
+        elif value["Vorname"] == "Urs":
+            try:
+                summe_auftritte_urs += float(value["event: auftritte"])
+            except:
+                continue
+        elif value["Vorname"] == "Cilli":
+            try:
+                summe_auftritte_cilli += float(value["event: auftritte"])
+            except:
+                continue
+
+    return render_template("berechnung.html",
+                           summe_proben_stefanie=summe_proben_stefanie,
+                           summe_proben_philipp=summe_proben_philipp,
+                           summe_proben_rene=summe_proben_rene,
+                           summe_proben_mario=summe_proben_mario,
+                           summe_proben_urs=summe_proben_urs,
+                           summe_proben_cilli=summe_proben_cilli,
+                           summe_auftritte_stefanie=summe_auftritte_stefanie,
+                           summe_auftritte_philipp=summe_auftritte_philipp,
+                           summe_auftritte_rene=summe_auftritte_rene,
+                           summe_auftritte_mario=summe_auftritte_mario,
+                           summe_auftritte_urs=summe_auftritte_urs,
+                           summe_auftritte_cilli=summe_auftritte_cilli,)
 
 
 if __name__ == "__main__":
